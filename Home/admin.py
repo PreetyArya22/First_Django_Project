9420845ceaf46b3b227aaad6d30bed004a0c4a3c
preety <preety.datacode@gmail.com>
2025-02-04 11:29:30 +0530

    create models

---
+++ Home/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Person , Musician , Album , Person_Choice , Runner , Fruit , Category , Car , Toyota , Manufacture
+
+# Register your models here.
+
+# admin.site.register(Person)
+# admin.site.register(Musician)
+# admin.site.register(Album)
+# admin.site.register(Person_Choice)
+# admin.site.register(Runner)
+# admin.site.register(Fruit)
+# admin.site.register(Category)
+admin.site.register(Car)
+admin.site.register(Toyota)
+admin.site.register(Manufacture)
---
+++ Home/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+
+# Create your models here.
+
+class Person (models.Model):
+  first_name = models.CharField(max_length=55)
+  last_name = models.CharField(max_length=55)
+
+class Musician(models.Model):
+  first_name = models.CharField(max_length=55)
+  last_name=models.CharField(max_length=55)
+  instrument = models.CharField(max_length=55)
+  
+class Album(models.Model):
+  artist = models.ForeignKey(Musician , on_delete=models.CASCADE)
+  name = models.CharField(max_length=100)
+  release_date = models.DateField()
+  num_starts = models.IntegerField()
+
+class Person_Choice(models.Model):
+  SHIRT_SIZE = {
+    "S" : "Small",
+    "M" : "Medium",
+    "L" : "Large", 
+  }
+  name = models.CharField(max_length=55)
+  shirt_size = models.CharField(max_length=55 , choices=SHIRT_SIZE)
+
+
+""" you can also use enumeration classes to define choices in a concise way: """
+
+class Runner(models.Model):
+  MedalType = models.TextChoices("MedalType", "GOLD SILVER BRONZE")
+  name = models.CharField(max_length=40)
+  medal = models.CharField(blank=True , choices = MedalType ,  max_length=10)
+  
+class Fruit(models.Model):
+  name = models.CharField(max_length=100 , primary_key=True)
+  type= models.CharField(max_length=100 , default="false" )
+  
+class Category(models.Model):
+  name = models.CharField(max_length=100)
+  fruit = models.ForeignKey(Fruit  , on_delete=models.CASCADE , verbose_name="related fruit")
+  
+class Car(models.Model):
+  model = models.CharField(max_length=100)
+  brand = models.CharField(max_length=100)
+
+class Toyota(models.Model):
+  car = models.OneToOneField(Car , on_delete= models.CASCADE , verbose_name="related car")
+  
+class Manufacture(models.Model):
+  car = models.ManyToManyField(Toyota , verbose_name="related toyota")
